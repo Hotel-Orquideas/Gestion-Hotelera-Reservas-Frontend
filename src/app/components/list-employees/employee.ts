4c90247f726eca8696c58b9f30d598884e9b061d
JulianC06 <william.castaneda03@uptc.edu.co>
2022-10-07 15:23:58 -0500

    terminando

--- src/app/components/list-employees/employee.ts
+++ src/app/components/list-employees/employee.ts
@@ -1,5 +1,6 @@
 export class Employee {
     id:number=0;
+    
     name:string="";
     lastName:string="";
     typeDocument:string="";
@@ -12,6 +13,19 @@ export class Employee {
     position:string="";
     userName:string="";
     password:string="";
+    
+    person={
+        "name":this.name,
+        "lastName":this.lastName,
+        "typeDocument":this.typeDocument,
+        "document":this.document,
+        "genre":this.genre,
+        "birthdate":this.birthdate,
+        "phoneNumber":this.phoneNumber,
+        "email":this.email,
+        "bloodType":this.bloodType
+    };
+    
     //roleId:number;
     //hotelId:number;
 
