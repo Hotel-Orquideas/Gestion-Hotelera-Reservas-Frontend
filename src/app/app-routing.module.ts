14a23244664988fc53d5b06c7d85ab9424ca5ae3
JulianC06 <william.castaneda03@uptc.edu.co>
2022-10-06 22:51:17 -0500

    PHO-18 registrar, listar visual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorNotFoundComponent } from './components/error-not-found/error-not-found.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
+import { RegisterEmployeeComponent } from './components/register-employee/register-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list-employees', component:ListEmployeesComponent},
+  {path:'register-employee', component:RegisterEmployeeComponent},
+  {path:'error-not-found',component:ErrorNotFoundComponent},
+  {path:'**', redirectTo:'error-not-found', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
