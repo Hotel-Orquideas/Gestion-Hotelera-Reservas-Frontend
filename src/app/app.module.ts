f335c4c0a529ea6a0c60d700bfd9b54069913d65
JulianC06 <william.castaneda03@uptc.edu.co>
2022-10-06 22:10:43 -0500

    PHO-18 creación de componentes y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
+import { RegisterEmployeeComponent } from './components/register-employee/register-employee.component';
+import { ErrorNotFoundComponent } from './components/error-not-found/error-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ListEmployeesComponent,
+    RegisterEmployeeComponent,
+    ErrorNotFoundComponent
   ],
   imports: [
     BrowserModule,
