16fbed4a4e977a258b3b26def74e73b4cfe4c4a0
JulianC06 <william.castaneda03@uptc.edu.co>
2022-10-23 18:58:48 -0500

    se sube la clase person

---
+++ src/app/components/list-employees/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+
+    name:string="";
+    lastName:string="";
+    typeDocument:string="";
+    document:string="";
+    genre:string="";
+    birthdate:Date=new Date();
+    phoneNumber:string="";
+    email:string="";
+    bloodType:string="";
+
+}
