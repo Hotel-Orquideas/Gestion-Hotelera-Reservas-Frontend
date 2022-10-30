7b62fb29f1707020bc8b2a62d1375eb446da584d
JulianC06 <william.castaneda03@uptc.edu.co>
2022-10-29 21:27:46 -0500

    PHO-52 Se termina register y update roomtype

--- src/app/components/roomType-components/register-room-type/register-room-type.component.ts
+++ src/app/components/roomType-components/register-room-type/register-room-type.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoomType } from '../list-room-types/roomType';
+import { MenuItem } from 'primeng/api';
+import { ToastrService } from 'ngx-toastr';
+import { RoomTypeService } from 'src/app/services/roomType-service/room-type.service';
 
 @Component({
   selector: 'app-register-room-type',
@@ -7,9 +13,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterRoomTypeComponent implements OnInit {
 
-  constructor() { }
+  public formRegister: FormGroup = new FormGroup<any>('');//para formulario registrar
+
+  roomType: RoomType = new RoomType();
+  items: MenuItem[] = new Array;//para breadcrumb
+  itemsElse: MenuItem[] = new Array;//para breadcrumb cuando es actualizar tipo de habitación
+  home: MenuItem = {};//para breadcrumb
+
+  constructor(private roomTypeService: RoomTypeService, private formBuilder: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+
+
+    //validaciones básicas para el formulario de registrar
+    this.formRegister = this.formBuilder.group({
+      name: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]],
+      numMaxGuests: ['', [
+        Validators.required,
+      ]]
+    });
+
+    this.cargar(); //llena el formulario si existe id en el parametro url
+
+    //etiquetas para el breadcrumb
+    this.items = [
+      { label: 'Tipo de habitación' },
+      { label: 'Registrar tipo de habitación' }
+    ];
+
+    //etiquetas para el breadcrumb cuando es actualziar servicio
+    this.itemsElse = [
+      { label: 'Servicio' },
+      { label: 'Tipos de habitaciones registradas', url: 'roomType/list-room-types' },
+      { label: 'Actualizar servicio' }
+    ];
+
+    //icono de casa pra el breadcrumb
+    this.home = { icon: 'pi pi-home', routerLink: '/' };
+
+  }
+
+  cargar(): void {
+
+    this.activatedRoute.params.subscribe(
+      rtype => {
+        let id = rtype['id'];
+        if (id) {
+          this.roomTypeService.getRoomType(id).subscribe(
+            rt => this.roomType = rt
+          );
+          
+        }
+      }
+    );
+
+  }
+
+  register(): void {
+
+    
+    this.roomTypeService.registerRoomType(this.roomType).subscribe(
+      res => {
+        this.toastr.success('El tipo de habitación se ha registrado satisfactoriamente.', 'Registro tipo habitación',{
+          closeButton:true,
+          progressBar:true
+        });
+        this.router.navigate(['/roomType/list-room-types'])}
+    );
+  }
+
+  update(): void {
+    this.roomTypeService.updateRoomType(this.roomType).subscribe(
+      emp => {
+        this.toastr.info('El tipo de habitación se ha actualizado satisfactoriamente.', 'Actualziar tipo habitación', {
+          closeButton:true,
+          progressBar:true
+        });
+        this.router.navigate(['/roomType/list-room-types'])}
+    );
   }
 
 }
