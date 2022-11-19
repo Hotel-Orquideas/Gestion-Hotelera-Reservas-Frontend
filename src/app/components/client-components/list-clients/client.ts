e8d10606e5075652bda2c6c9aaea9817196c2a2c
JulianC06 <william.castaneda03@uptc.edu.co>
2022-11-19 15:37:14 -0500

    PHO-55 Clase client

---
+++ src/app/components/client-components/list-clients/client.ts
@@ -0,0 +1,12 @@
+import { Person } from "../../employee-components/list-employees/person";
+
+export class Client{
+    id:number;
+    country_origin:string;
+    city_origin:string;
+    country_destination:string;
+    city_destination:string;
+    document_expedition_date:Date;
+    profession:string;
+    person:Person = new Person();
+}
